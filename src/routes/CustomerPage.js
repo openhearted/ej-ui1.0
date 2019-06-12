74354093c0de82d77712397567a628edc691f452
虎虎胖胖 <1550489246@qq.com>
2019-06-12 14:41:33 +0800

    冯萌-实现了waiter的删除和批量删除的功能

--- src/routes/CustomerPage.js
+++ src/routes/CustomerPage.js
@@ -2,8 +2,11 @@ import React from 'react';
 // 引入css进行页面美化
 import styles from './IndexPage.css'
 // 导入组件
-import {Modal,Button,Table,message} from 'antd'
+// 导入组件
+import {Modal,Button, Table,message} from 'antd'
 import axios from '../utils/axios'
+import CustomerForm from './CustomerForm'
+
 
 // 组件类必须要继承React.Component，是一个模块，顾客管理子功能
 class CustomerPage extends React.Component {
@@ -11,9 +14,11 @@ class CustomerPage extends React.Component {
   constructor(){
     super();
     this.state = {
-      ids:[],//批量删除的时候保存的id
+      ids:[], // 批量删除的时候保存的id
       list:[],
-      loading:false
+      loading:false,
+      visible:false,
+      customer:{}
     }
   }
   // 在生命周期钩子函数中调用重载数据
@@ -33,75 +38,111 @@ class CustomerPage extends React.Component {
       this.setState({loading:false});
     })
   }
-  //批量删除
+  // 批量删除
   handleBatchDelete(){
     Modal.confirm({
-      title: '你确认删除这些记录吗?',
-      content: '一旦确认将无法恢复',
-      okText: '确认',
-      okType: 'danger',
-      cancelText: '取消',
-      onOk:()=>{
+      title: '确定删除这些记录吗?',
+      content: '一旦删除数据将无法恢复',
+      onOk:() => {
         axios.post("/customer/batchDelete",{ids:this.state.ids})
         .then((result)=>{
+          //批量删除后重载数据
           message.success(result.statusText)
-          this.reloadData()
+          this.reloadData();
         })
-      },
+      }
     });
   }
-  //删除
+
+  // 单个删除
   handleDelete(id){
-    //传递到后台
     Modal.confirm({
-      title: '你确认删除这条记录吗?',
-      content: '一旦确认将无法恢复',
-      okText: '确认',
-      okType: 'danger',
-      cancelText: '取消',
-      onOk:()=>{
+      title: '确定删除这条记录吗?',
+      content: '一旦删除数据将无法恢复',
+      onOk:() => {
+        // 删除操作
         axios.get("/customer/deleteById",{
           params:{
             id:id
           }
         })
         .then((result)=>{
-          message.success(result.statusText)
-          this.reloadData()
+          // 删除成功后提醒消息，并且重载数据
+          message.success(result.statusText);
+          this.reloadData();
         })
-      },
+      }
     });
+  }
+
+  // 取消按钮的事件处理函数
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+  // 确认按钮的事件处理函数
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+      // 表单校验完成后与后台通信进行保存
+      axios.post("/customer/saveOrUpdate",values)
+      .then((result)=>{
+        message.success(result.statusText)
+        // 重置表单
+        form.resetFields();
+        // 关闭模态框
+        this.setState({ visible: false });
+        this.reloadData();
+      })
+    });
+  };
+  // 将子组件的引用在父组件中进行保存，方便后期调用
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+  // 去添加
+  toAdd(){
+    // 将默认值置空,模态框打开
+    this.setState({customer:{},visible:true})
+  }
+  // 去更新
+  toEdit(record){
+    // 更前先先把要更新的数据设置到state中
+    this.setState({customer:record})
+    // 将record值绑定表单中
+    this.setState({visible:true})
   }
 
   // 组件类务必要重写的方法，表示页面渲染
   render(){
     // 变量定义
-    let columns = [
-      {
-        title:'姓名',
-        dataIndex:'realname'
-      },{
-        title:'手机号',
-        dataIndex:'telephone'
-      },{
-        title:'状态',
-        dataIndex:'status'
-      },{
+    let columns = [{
+      title:'姓名',
+      dataIndex:'realname'
+    },{
+      title:'手机号',
+      dataIndex:'telephone'
+    },{
+      title:'状态',
+      dataIndex:'status'
+    },{
       title:'操作',
-      width:200,
+      width:120,
       align:"center",
       render:(text,record)=>{
         return (
           <div>
-            <Button type='link' size="small" onClick = {this.handleDelete.bind(this,record.id)}>删除</Button>
-            <Button type='link' size="small">修改</Button>
+            <Button type='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
+            <Button type='link' size="small" onClick={this.toEdit.bind(this,record)}>修改</Button>
           </div>
         )
       }
     }]
     const rowSelection = {
       onChange: (selectedRowKeys, selectedRows) => {
-        //当用户操作复选按钮的时候，将值获取并且保存到state中
+        // 当用户操作复选按钮的时候，将值获取到并且保存到state中
         this.setState({
           ids:selectedRowKeys
         })
@@ -117,8 +158,8 @@ class CustomerPage extends React.Component {
       <div className={styles.customer}>
         <div className={styles.title}>顾客管理</div>
         <div className={styles.btns}>
-          <Button>添加</Button> &nbsp;
-          <Button onClick = {this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
+          <Button onClick={this.toAdd.bind(this)}>添加</Button> &nbsp;
+          <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
           <Button type="link">导出</Button>
         </div>
         <Table 
@@ -129,6 +170,13 @@ class CustomerPage extends React.Component {
           rowSelection={rowSelection}
           columns={columns}
           dataSource={this.state.list}/>
+
+        <CustomerForm
+          initData={this.state.customer}
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}/>
       </div>
     )
   }
